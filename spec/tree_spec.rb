d3f9398eaf6bd029a76a6e4d64c58e338e594a75
nagraham <nagraham00@hotmail.com>
2016-02-09 07:45:14 -0800

    Add tests for Tree#each and its various algorithms

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -17,13 +17,8 @@ describe Tree do
     end
     context 'when multiple key/values are added to the tree' do
       before do
-        tree.add(:a, 1)
-        tree.add(:b, 2)
-        tree.add(:c, 3)
-        tree.add(:d, 4)
-        tree.add(:e, 5)
-        tree.add(:f, 6)
-        tree.add(:g, 7)
+        pairs = [[:a, 1], [:b, 2], [:c, 3], [:d, 4], [:e, 5], [:f, 6], [:g, 7]]
+        pairs.each { |p| tree.add(p.first, p.last) }
       end
       it 'will add the values in a breath-first manner' do
         expect(tree.root.value).to eq(1)
@@ -37,19 +32,59 @@ describe Tree do
     end
   end
   
-  # describe '#each' do
-  #   let(:tree) { Tree.new }
-  #   before do
-  #     tree.add(:b, 23)
-  #     tree.add(:c, 12)
-  #     tree.add(:a, 4)
-  #     tree.add(:e, 64)
-  #     tree.add(:g, 78)
-  #     tree.add()
-  #   end
-  #   context 'with in order algorithm' do
-  #
-  #   end
-  # end
-  
+  # Tree:
+  #        a
+  #    b        c
+  #  d   e    f   g
+  describe '#each' do
+    let(:tree) { Tree.new }
+    before do
+      pairs = [[:a, 1], [:b, 2], [:c, 3], [:d, 4], [:e, 5], [:f, 6], [:g, 7]]
+      pairs.each { |p| tree.add(p.first, p.last) }
+    end
+    context 'with the in-order algorithm' do
+      let(:target_values) { [4, 2, 5, 1, 6, 3, 7] }
+      let(:in_order_values) do
+        values = []
+        tree.each(:in_order) { |key, value| values << value }
+        values
+      end
+      it 'should iterate through the tree in an "in-order" sequence' do
+        expect(in_order_values).to eq(target_values)
+      end
+    end
+    context 'with the pre-order algorithm' do
+      let(:target_values) { [1, 2, 4, 5, 3, 6, 7] }
+      let(:pre_order_values) do
+        values = []
+        tree.each(:pre_order) { |key, value| values << value }
+        values
+      end
+      it 'should iterate through the tree in a "pre-order" sequence' do
+        expect(pre_order_values).to eq(target_values)
+      end
+    end
+    context 'with the post-order algorithm' do
+      let(:target_values) { [4, 5, 2, 6, 7, 3, 1] }
+      let(:post_order_values) do
+        values = []
+        tree.each(:post_order) { |key, value| values << value }
+        values
+      end
+      it 'should iterate through the tree in a "post-order" sequence' do
+        expect(post_order_values).to eq(target_values)
+      end
+    end
+    context 'with the breadth-first algorithm' do
+      let(:target_values) { [1, 2, 3, 4, 5, 6, 7] }
+      let(:breadth_first_values) do
+        values = []
+        tree.each(:breadth_first) { |key, value| values << value }
+        values
+      end
+      it 'should iterate through the tree in a "breadth-first" sequence' do
+        expect(breadth_first_values).to eq(target_values)
+      end
+    end
+  end
 end
