58908c96fbfa4bfe86a3562fc002d9a0c5fc7864
nagraham <nagraham00@hotmail.com>
2016-02-18 14:44:12 -0800

    Pull out boolean logic from RedBlackTree#add, to RBNode instance methods, for readability

--- lib/red_black_tree.rb
+++ lib/red_black_tree.rb
@@ -1,6 +1,12 @@
-require_relative 'tree'
+require_relative 'binary_tree'
 
-class RedBlackTree < Tree
+# The RedBlackTree uses red/black nodes to simulate 2-3 search trees. This 
+# datastructure maintains balance by maintaining the following invariants:
+#  - All invariants of a standard BST
+#  - Only left-child nodes can be red
+#  - You cannot have a chain of two red nodes in a row
+# These invariants are maintained through rotation algorithms. 
+class RedBlackTree < BinaryTree
 
   def add(key, value = nil)
     @root = add_to_tree(@root, RBNode.new(key: key, value: value))
@@ -10,6 +16,7 @@ class RedBlackTree < Tree
   
   private
   
+
   def add_to_tree(current_node, new_node)
     if current_node.nil?
       current_node = new_node
@@ -24,25 +31,25 @@ class RedBlackTree < Tree
         current_node.value = new_node.value
       end
       
-      if is_red?(current_node.right_node) && !is_red?(current_node.left_node)
+      if current_node.right_leaning_red_node?
         current_node = rotate_left(current_node)
       end
       
-      if is_red?(current_node.left_node) && is_red?(current_node.left_node.left_node)
+      if current_node.two_red_nodes_in_row?
         current_node = rotate_right(current_node)
       end
       
-      if is_red?(current_node.right_node) && is_red?(current_node.left_node)
+      if current_node.both_children_red?
         current_node.flip_colors
       end
       
       return current_node
     end
   end
-  
+
   def rotate_left(node)
     return node if node.right_node.nil?
-    
+
     new_parent_node = node.right_node
     node.right_node = new_parent_node.left_node
     new_parent_node.left_node = node
@@ -50,20 +57,16 @@ class RedBlackTree < Tree
     node.to_red
     return new_parent_node
   end
-  
+
   def rotate_right(node)
     return node if node.left_node.nil?
-    
+
     new_parent_node = node.left_node
     node.left_node = new_parent_node.right_node
     new_parent_node.right_node = node
     new_parent_node.color = node.color
     node.to_red
     return new_parent_node
-  end
-  
-  def is_red?(node)
-    !node.nil? && node.red?
   end
   
   ### RED BLACK NODE ###
@@ -77,13 +80,13 @@ class RedBlackTree < Tree
       super(params)
       @color = params[:color] || RED
     end
-    
-    def black?
-      @color == BLACK
-    end
-    
+
     def red?
       @color == RED
+    end
+    
+    def child_red?(child)
+      !child.nil? && child.red?
     end
     
     def to_black
@@ -98,6 +101,18 @@ class RedBlackTree < Tree
       self.to_red
       self.right_node.to_black unless self.right_node.nil?
       self.left_node.to_black unless self.left_node.nil?
+    end
+    
+    def right_leaning_red_node?
+      child_red?(right_node) && !child_red?(left_node)
+    end
+    
+    def two_red_nodes_in_row?
+      child_red?(left_node) && child_red?(left_node.left_node)
+    end
+    
+    def both_children_red?
+      child_red?(left_node) && child_red?(right_node)
     end
   end
 end
